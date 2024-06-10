cb3afb30ce1ec220bb4e282e6914df8bedf07055
Robson-Jr <robsonjuniorsn@gmail.com>
2024-06-10 17:13:16 -0300

    removendo arquivos desnecessarios

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="otica, vida, devmedia, react"/>
         <meta name="author" content="Robson-Lima-Jr"/>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
   );
