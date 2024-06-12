768be736ff8d360796d7d2f71a7f23bfcdc4cfa3
Robson-Jr <robsonjuniorsn@gmail.com>
2024-06-12 20:03:10 -0300

    concluindo componente secaoContatos e correção de flexbox do componente secaoSobre

--- src/components/SecaoContatos/index.jsx
+++ src/components/SecaoContatos/index.jsx
@@ -15,7 +15,43 @@ export default function SecaoContatos() {
 
             <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.</p>
 
-            <div></div>
+            <div className={styles.contato_box}>
+                <div>
+                    <h5>Contato</h5>
+                    <div className={styles.contato_descricao}>
+                        <div>
+                            <Image src={Local_icon} alt="local" className={styles.icones_contato} />
+                            <p>Nova Iguaçu, RJ</p>
+                        </div>
+                        <div>
+                            <Image src={Telefone_icon} alt="telefone" className={styles.icones_contato} />
+                            <p>(21) 9999-9999</p>
+                        </div>
+                        <div>
+                            <Image src={Email_icon} alt="email" className={styles.icones_contato} />
+                            <p>contato@oticavida.com</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h5>Nossas Redes Sociais</h5>
+                    <div className={styles.contato_descricao}>
+                        <div>
+                            <Image src={Facebook_icon} alt="facebook" className={styles.icones_contato} />
+                            <p>/oticaVida</p>
+                        </div>
+                        <div>
+                            <Image src={Intagram_icon} alt="instagram" className={styles.icones_contato} />
+                            <p>@oticavidarj</p>
+                        </div>
+                        <div>
+                            <Image src={Twitter_icon} alt="twitter" className={styles.icones_contato} />
+                            <p>@oticavidarj</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
